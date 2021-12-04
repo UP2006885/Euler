f007000b2823e3d60dc9bdb8ad215a581e5c0745
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2021-12-04 21:56:19 +0000

    Created a basic solution using a for loop to output a Fibonacci sequence based on a integer of the max size

--- ProblemTwo/evenFibonacciNums/evenFibonacciNums.java
+++ ProblemTwo/evenFibonacciNums/evenFibonacciNums.java
@@ -1,5 +1,18 @@
 public class evenFibonacciNums {
     public static void main(String[] args) {
-        
+        // 1, 2. (1+2)3, then (2+3)5 then (5+3)8 etc.
+        int iAmountOfTerms = 10;
+        int iCurrentTerm = 2;
+        int iTempTerm = 0;
+        int iPreviousTerm = 1;
+
+        for(int i = 0; i <iAmountOfTerms; i++){
+            System.out.println(iPreviousTerm);
+            System.out.println(iCurrentTerm);
+
+            iTempTerm = iCurrentTerm;
+            iCurrentTerm += iPreviousTerm;
+            iPreviousTerm = iTempTerm;
+        }
     }
 }
