ba2c021545845445165adafd74a2831b2a6163a1
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2021-12-04 21:20:36 +0000

    Added a simple solution to the base example, using for loops

--- ProblemOne/MultiplesOf3or5/MultiplesOf3or5.java
+++ ProblemOne/MultiplesOf3or5/MultiplesOf3or5.java
@@ -1,5 +1,27 @@
 public class MultiplesOf3or5{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int iNumber = 10; // This is the maximum number for this problem.
+        int iSum3 = 0;
+        int iSum5 = 0;
+        // 3/5. We must find all multiples of 3 and 5 inside this maximum number, and then sum them together.
+
+
+        for(int i = 1; i < iNumber; i++){ // %3
+            if((i % 3)== 0) {
+                System.out.println(i + " Is a multiple of 3.");
+                iSum3 = iSum3 + i;
+            }
+        } 
+
+        for(int i = 1; i < iNumber; i++){ // %5
+            if((i % 5)== 0) {
+                System.out.println(i + " Is a multiple of 5.");
+                iSum5 = iSum5 + i;
+            }
+        } 
+
+        System.out.println("The total sum is: "+ (iSum3 + iSum5));
+
+
     }
 }
