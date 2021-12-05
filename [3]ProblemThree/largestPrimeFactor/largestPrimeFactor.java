6aaea5563052980d6285b52a9af5a9a74a0083f7
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2021-12-05 17:31:44 +0000

    Added a basic java loop to find the factors of a specified integer.

--- [3]ProblemThree/largestPrimeFactor/largestPrimeFactor.java
+++ [3]ProblemThree/largestPrimeFactor/largestPrimeFactor.java
@@ -1,5 +1,14 @@
 public class largestPrimeFactor{
     public static void main(String[] args) {
-        
+        int iFactorOF = 32;
+
+        for(int i = 1; i <= iFactorOF; i++){
+            /* Simply a factor is a number in which can be devided with its factor parent, to create a whole number. 
+            For example 2 is a factor of 8, because the result is 4. 3 is not a factor as the result would have a decimal value. */
+            if((iFactorOF % i) == 0){
+                System.out.println("A factor of " + iFactorOF + " is: " +i);
+            }
+        }
+
     }
 }
